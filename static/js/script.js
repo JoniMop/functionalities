a100bf63dabbd0b68e126e6aac157a96a0271833
JoniMop <jonatangurari@gmail.com>
2018-05-16 00:20:38 -0600

    My 2nd commit

--- static/js/script.js
+++ static/js/script.js
@@ -1,27 +1,106 @@
-//alert('hi');
-//console.log('hello');
-
-//Challenge 1 below, your age in days.
+// Challenge 1: Your Age in Days
 function ageInDays() {
-  var birthYear = prompt('What Year Were You Born?');
-  var ageInDias = (2018 - birthYear) * 365;
-  var h1 = document.createElement('h1');
-  var textAnswer = document.createTextNode('You are ' + ageInDias + ' days old');
+  var birthYear = prompt("What year were you born... Good friend?");
+  var ageInDays = (2018 - birthYear) * 365;
+  var h1 = document.createElement('H1');
+  var textAnswer = document.createTextNode('You are ' + ageInDays.toString() + ' days old.');
   h1.setAttribute('id', 'ageInDays');
   h1.appendChild(textAnswer);
   document.getElementById('flex-box-result').appendChild(h1);
-  console.log(ageInDias);
 }
 
 function reset() {
   document.getElementById('ageInDays').remove();
-
 }
 
-//Challenge 2 below, cat generator GIF
+// Challenge 2: Cat Generator
 function generateCat() {
-  var image = document.createElement('img');
-  var div = document.getElementById('flex-cat-gen');
-  image.src = "https://78.media.tumblr.com/tumblr_m0lbulQH4r1r94e9jo1_250.gif"
-  div.appendChild(image);
+  var catDiv = document.createElement('div');
+  catDiv.innerHTML = "<img class='cat-image' src='http://thecatapi.com/api/images/get?format=src&type=gif&size=small'>"
+  document.getElementById('catGeneratorDiv').appendChild(catDiv);
+}
+
+
+// Challenge 3: Rock, Paper, Scissors
+
+function rpsGame(yourChoice) {
+  var humanChoice, botChoice;
+  humanChoice = yourChoice.id;
+  botChoice = numberToChoice(randToRpsInt());
+  results = decideWinner(humanChoice, botChoice);
+  message = finalMessage(results)
+  rpsFrontEnd(yourChoice.id, botChoice, message);
+  // alert(message + '\n---------------------------\n\nYou: ' + humanChoice + '\nBot: ' + botChoice + '\nYour Score: ' + humanScore + '\nBot Score: ' + botScore);
+}
+
+/**
+ * Returns either 0, 1 or 2
+ */
+function randToRpsInt() {
+  return Math.floor(Math.random() * 3)
+}
+
+/**
+ * Takes in a integer and returns the choice as a string.
+ * @param {int} number - Numbers 0-2
+ */
+function numberToChoice(number) {
+  return ['rock', 'paper', 'scissors'][number]
+}
+
+/**
+ * Takes in your choice & computer's choice as strings and returns
+ * the scores as an array.
+ * @param {string} yourChoice
+ * @param {string} computerChoice
+ */
+function decideWinner(yourChoice, computerChoice) {
+  var rpsDatabase = {
+    'rock': {'scissors': 1, 'rock': 0.5, 'paper': 0},
+    'paper': {'rock': 1, 'paper': 0.5, 'scissors': 0},
+    'scissors': {'paper': 1, 'scissors': 0.5, 'rock': 0},
+  }
+
+  var yourScore = rpsDatabase[yourChoice][computerChoice]
+  var computerScore = rpsDatabase[computerChoice][yourChoice]
+
+  return [yourScore, computerScore]
+}
+
+function finalMessage([yourScore, computerScore]){
+  if (yourScore === 0) {
+    return {'message': 'You lost!', 'color': 'red'}
+  } else if (yourScore == 0.5) {
+    return {'message': 'You tied!', 'color': 'yellow'}
+  } else {
+    return {'message': 'You won!', 'color': 'green'}
+  }
+}
+
+function rpsFrontEnd(humanImageChoice, botImageChoice, finalMessage) {
+  var items = ['rock', 'paper', 'scissors'];
+
+  var imagesDatabase = {
+    'rock': document.getElementById('rock').src,
+    'paper': document.getElementById('paper').src,
+    'scissors': document.getElementById('scissors').src
+  };
+
+  document.getElementById('rock').remove();
+  document.getElementById('paper').remove();
+  document.getElementById('scissors').remove();
+
+  var humanDiv = document.createElement('div');
+  var botDiv = document.createElement('div');
+  var messageDiv = document.createElement('div');
+
+  humanDiv.innerHTML = "<img src='" + imagesDatabase[humanImageChoice] + "' height=150 width=150 style='box-shadow: 0px 10px 50px rgba(37, 50, 233, 1);'>"
+  messageDiv.innerHTML = "<h1 style='color: " + finalMessage['color'] + "; font-size: 60px; padding: 30px; '>" + finalMessage['message'] + "</h1>"
+  botDiv.innerHTML = "<img src='" + imagesDatabase[botImageChoice] + "' height=150 width=150 style='box-shadow: 0px 10px 50px rgba(243, 38, 24, 1);'>"
+
+  document.getElementById('flex-box-rps-div').appendChild(humanDiv);
+  document.getElementById('flex-box-rps-div').appendChild(messageDiv);
+  document.getElementById('flex-box-rps-div').appendChild(botDiv);
+
+
 }
